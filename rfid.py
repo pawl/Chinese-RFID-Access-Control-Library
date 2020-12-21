ed51508c0526497cba659e8666bdbf0a8158039c
Paul Brown <paul90brown@gmail.com>
2020-12-20 20:02:36 -0600

    format with isort and black

--- rfid.py
+++ rfid.py
@@ -1,5 +1,5 @@
-import socket
 import binascii
+import socket
 import struct
 import sys
 
@@ -26,12 +26,13 @@ def ten_digit_to_comma_format(badge):
     # the 8 digits correspond to only 6 hex values, so the max is FFFFFF
     if badge > 16777215:
         raise Exception("Error: Invalid RFID Number")
-    formattedID = str("{0:x}".format(badge)).zfill(6)  # converts to hex
+    formatted_id = str("{0:x}".format(badge)).zfill(6)  # converts to hex
 
     # splits the hex at first two and last 4, converts to dec,
     # then combines into string
-    return int(str(int(formattedID[:2], 16)).zfill(3) +
-               str(int(formattedID[-4:], 16)).zfill(5))
+    id_section_1 = str(int(formatted_id[:2], 16)).zfill(3)
+    id_section_2 = str(int(formatted_id[-4:], 16)).zfill(5)
+    return int(id_section_1 + id_section_2)
 
 
 def comma_format_to_ten_digit(badge):
@@ -62,20 +63,22 @@ def comma_format_to_ten_digit(badge):
 
     # splits dec at last 5 digits and everything except last 5,
     # converts each section to hex, then combines
-    formattedID = ("{0:x}".format(int(badge[:-5])).zfill(2) +
-                   "{0:x}".format(int(badge[-5:])).zfill(4))
+    id_section_1 = "{0:x}".format(int(badge[:-5])).zfill(2)
+    id_section_2 = "{0:x}".format(int(badge[-5:])).zfill(4)
+    formatted_id = id_section_1 + id_section_2
 
     # convert combined hex string to int
-    return int(formattedID, 16)
+    return int(formatted_id, 16)
 
 
 class RFIDClient(object):
     # part of the byte string replaced by the CRC, not required to be valid
-    source_port = '0000'
+    source_port = "0000"
 
     # this byte starts a transaction
-    start_transaction = ('0d0d000000000000000000000000000000000000000'
-                         '0000000000000'.decode('hex'))
+    start_transaction = (
+        "0d0d0000000000000000000000000000000000000000000000000000".decode("hex")
+    )
 
     def __init__(self, ip, serial):
         """
@@ -89,7 +92,7 @@ class RFIDClient(object):
             raise TypeError("Serial must be set to an integer")
 
         # pack controller serial as little endian integer
-        self.controller_serial = struct.pack('<I', serial).encode('hex')
+        self.controller_serial = struct.pack("<I", serial).encode("hex")
         self.s = self.connect(ip)
 
     @staticmethod
@@ -117,7 +120,7 @@ class RFIDClient(object):
 
     @staticmethod
     def crc_16_ibm(data):
-        """ Returns hex string with CRC values added to positions 4 through 8.
+        """Returns hex string with CRC values added to positions 4 through 8.
         This CRC value is required by the controller or it will not process the
         request.
 
@@ -143,7 +146,7 @@ class RFIDClient(object):
 
         # switch order to little endian and return unsigned short, then
         # replace characters in list with the CRC values
-        data_list[4:8] = struct.pack('<H', code).encode('hex')
+        data_list[4:8] = struct.pack("<H", code).encode("hex")
 
         return "".join(data_list)
 
@@ -164,31 +167,42 @@ class RFIDClient(object):
                 doors_enabled += "00"
 
         # pack badge number as little endian integer
-        badge = struct.pack('<I', badge).encode('hex')
+        badge = struct.pack("<I", badge).encode("hex")
 
         add_packet1 = self.crc_16_ibm(
-            '2010' + self.source_port + '2800000000000000' +
-            self.controller_serial + '00000200ffffffff'
-        ).decode('hex')
+            "2010"
+            + self.source_port
+            + "2800000000000000"
+            + self.controller_serial
+            + "00000200ffffffff"
+        ).decode("hex")
 
         self.s.send(self.start_transaction)
         self.s.send(add_packet1)
         recv_data1 = binascii.b2a_hex(self.s.recv(1024))
 
-        if (recv_data1[:4] != '2011'):
+        if recv_data1[:4] != "2011":
             raise Exception("Unexpected Result Received: %s" % recv_data1)
 
         add_packet2 = self.crc_16_ibm(
-            '2320' + self.source_port + '2900000000000000' +
-            self.controller_serial + '00000200' + badge +
-            '00000000a04e4605' + '87' + '1c9f3b' + doors_enabled + '00000000'
-        ).decode('hex')
+            "2320"
+            + self.source_port
+            + "2900000000000000"
+            + self.controller_serial
+            + "00000200"
+            + badge
+            + "00000000a04e4605"
+            + "87"
+            + "1c9f3b"
+            + doors_enabled
+            + "00000000"
+        ).decode("hex")
 
         self.s.send(self.start_transaction)
         self.s.send(add_packet2)
         recv_data2 = binascii.b2a_hex(self.s.recv(1024))
 
-        if (recv_data2[:4] != '2321'):
+        if recv_data2[:4] != "2321":
             raise Exception("Unexpected Result Received: %s" % recv_data2)
 
     def remove_user(self, badge):
@@ -196,19 +210,23 @@ class RFIDClient(object):
             raise TypeError("RFID number must be set to an integer")
 
         # pack badge number as little endian integer
-        badge = struct.pack('<I', badge).encode('hex')
+        badge = struct.pack("<I", badge).encode("hex")
 
         remove_packet = self.crc_16_ibm(
-            '2320' + self.source_port + '2200000000000000' +
-            self.controller_serial + '00000200' + badge +
-            '00000000204e460521149f3b0000000000000000'
-        ).decode('hex')
+            "2320"
+            + self.source_port
+            + "2200000000000000"
+            + self.controller_serial
+            + "00000200"
+            + badge
+            + "00000000204e460521149f3b0000000000000000"
+        ).decode("hex")
 
         self.s.send(self.start_transaction)
         self.s.send(remove_packet)
         recv_data = binascii.b2a_hex(self.s.recv(1024))
 
-        if (recv_data[:4] != '2321'):
+        if recv_data[:4] != "2321":
             raise Exception("Unexpected Result Received: %s" % recv_data)
 
     def open_door(self, door_number):
@@ -220,21 +238,25 @@ class RFIDClient(object):
         door_number = str(door_number - 1).zfill(2)
 
         open_door_packet = self.crc_16_ibm(
-            '2040' + self.source_port + '0500000000000000' +
-            self.controller_serial + '0000020001000000ffffffffffffffff' +
-            door_number + '000000'
-        ).decode('hex')
+            "2040"
+            + self.source_port
+            + "0500000000000000"
+            + self.controller_serial
+            + "0000020001000000ffffffffffffffff"
+            + door_number
+            + "000000"
+        ).decode("hex")
 
         self.s.send(self.start_transaction)
         self.s.send(open_door_packet)
         recv_data = binascii.b2a_hex(self.s.recv(1024))
 
-        if (recv_data[:4] != '2041'):
+        if recv_data[:4] != "2041":
             raise Exception("Unexpected Result Received: %s" % recv_data)
 
     def __del__(self):
         """
         Closes the socket connection.
         """
-        if hasattr(self, 's'):
+        if hasattr(self, "s"):
             self.s.close()
--- tests.py
+++ tests.py
@@ -1,11 +1,12 @@
 import unittest
+
 from mock import patch
 
-from rfid import comma_format_to_ten_digit, ten_digit_to_comma_format, RFIDClient
+from rfid import (RFIDClient, comma_format_to_ten_digit,
+                  ten_digit_to_comma_format)
 
 
 class TestTenDigitToCommaFormat(unittest.TestCase):
-
     def test_happy_path(self):
         result = ten_digit_to_comma_format(11111111)
         expected_result = 16935527
@@ -13,7 +14,6 @@ class TestTenDigitToCommaFormat(unittest.TestCase):
 
 
 class TestCommaFormatToTenDigit(unittest.TestCase):
-
     def test_happy_path(self):
         result = comma_format_to_ten_digit(16935527)
         expected_result = 11111111
@@ -21,26 +21,28 @@ class TestCommaFormatToTenDigit(unittest.TestCase):
 
 
 class TestRFIDClient(unittest.TestCase):
-
     def test_invalid_ip(self):
         with self.assertRaises(TypeError):
-            RFIDClient('blah')
+            RFIDClient("blah")
 
-    @patch('rfid.RFIDClient.connect')
-    @patch('rfid.RFIDClient.check_valid_ipv4_address')
+    @patch("rfid.RFIDClient.connect")
+    @patch("rfid.RFIDClient.check_valid_ipv4_address")
     def test_controller_serial(self, mock_connect, mock_check_valid_ipv4_address):
-        controller_ip = '192.168.1.20'
+        controller_ip = "192.168.1.20"
         controller_serial = 123106461
         rfid_client = RFIDClient(controller_ip, controller_serial)
-        expected_controller_serial = '9d745607'
+        expected_controller_serial = "9d745607"
         self.assertEqual(rfid_client.controller_serial, expected_controller_serial)
 
     def test_crc_16_ibm(self):
-        test_controller_serial = '9d745607'
+        test_controller_serial = "9d745607"
         test_data = (
-            '2010' + RFIDClient.source_port + '2800000000000000' +
-            test_controller_serial + '00000200ffffffff'
+            "2010"
+            + RFIDClient.source_port
+            + "2800000000000000"
+            + test_controller_serial
+            + "00000200ffffffff"
         )
         result = RFIDClient.crc_16_ibm(test_data)
-        expected_result = '201066f228000000000000009d74560700000200ffffffff'
+        expected_result = "201066f228000000000000009d74560700000200ffffffff"
         self.assertEqual(result, expected_result)
