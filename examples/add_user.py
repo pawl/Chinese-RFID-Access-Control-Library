aabe15ac92052507dffc660debd1b5648dbd75c0
Paul Brown <paul90brown@gmail.com>
2020-12-21 00:17:47 -0600

    format examples with black

--- examples/add_user.py
+++ examples/add_user.py
@@ -1,12 +1,13 @@
 from rfid import ten_digit_to_comma_format, RFIDClient
 
-ip_address = '192.168.1.20'
+ip_address = "192.168.1.20"
 controller_serial = 123106461
 client = RFIDClient(ip_address, controller_serial)
 
-badge = ten_digit_to_comma_format(11111111) # badge number needs to be in "comma format"
+# badge number needs to be in "comma format"
+badge = ten_digit_to_comma_format(11111111)
 
-client.add_user(badge, [1]) # add badge to door 1
-client.add_user(badge, [1, 2]) # add badge to door 1 and 2
-client.add_user(badge, [1, 2, 3]) # add badge to door 1, 2, and 3
-client.add_user(badge, [1, 2, 3, 4]) # add badge to door 1, 2, 3, and 4
+client.add_user(badge, [1])  # add badge to door 1
+client.add_user(badge, [1, 2])  # add badge to door 1 and 2
+client.add_user(badge, [1, 2, 3])  # add badge to door 1, 2, and 3
+client.add_user(badge, [1, 2, 3, 4])  # add badge to door 1, 2, 3, and 4
--- examples/open_door.py
+++ examples/open_door.py
@@ -1,6 +1,6 @@
 from rfid import RFIDClient
 
-ip_address = '192.168.1.20'
+ip_address = "192.168.1.20"
 controller_serial = 123106461
 client = RFIDClient(ip_address, controller_serial)
 
